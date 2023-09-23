bb0fcd3c0c2f02e66b3c9fa436923a7fc0785ac9
Zahar Kudriavcev <zaharkud162@gmail.com>
2023-09-23 20:07:12 +0300

    Server changes

--- src/services/consts/app.consts.ts
+++ src/services/consts/app.consts.ts
@@ -1,1 +1,1 @@
-export const REACT_APP_API_URL = "http://45.146.166.9:5000/";//"https://arlezu-server.onrender.com";
+export const REACT_APP_API_URL = "http://45.146.166.9:5000";//"https://arlezu-server.onrender.com";
